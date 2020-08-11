2f60cdac986303a40f90ca5e51b71bdbd69925b6
ashak299 <ashak299@users.noreply.github.com>
2020-08-11 17:51:22 +0100

    Create Inital Django App

--- project/project/urls.py
+++ project/project/urls.py
@@ -3,6 +3,7 @@ Definition of urls for project.
 """
 
 from django.conf.urls import include, url
+import App.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,4 +19,7 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', App.views.index, name='index'),
+    url(r'^home$', App.views.index, name='home'),
 ]

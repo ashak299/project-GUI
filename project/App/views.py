6966c63142be817799d5d9309bab3beed372a65f
ashak299 <ashak299@users.noreply.github.com>
2020-08-11 22:46:56 +0100

    Added Encryption and decryption pages

--- project/App/views.py
+++ project/App/views.py
@@ -13,7 +13,30 @@ def index(request):
             'title' : "Start Page",
             'message' : "Choose Encryption or Decryption",
             'encryptionButton' : "Encryption",
-            'decryptionButton' : "Decryption" 
+            'decryptionButton' : "Decryption", 
+            'extensionMessage' : "Files to be encrypted must have extension: .txt, .docx and decryption requires files with extension: .enc",
             
+        }
+    )
+
+def EncryptPage(request):
+    return render(
+        request,
+        "App/EncryptPage.html",
+        {
+            'title' : "Encrypt Page",
+            'chooseFile' : "Encryption",
+            'extensionMessage' : "Files to be encrypted must have extension: .txt, .docx"
+        }
+    )
+
+def DecryptPage(request):
+    return render(
+        request,
+        "App/DecryptPage.html",
+        {
+            'title' : "Decrypt Page",
+            'head' : "Decryption",
+            'extensionMessage' : "Files to be decrypted must have extension: .enc"
         }
     )
--- project/project/urls.py
+++ project/project/urls.py
@@ -3,6 +3,8 @@ Definition of urls for project.
 """
 
 from django.conf.urls import include, url
+from App import views
+from App.views import EncryptPage
 import App.views
 
 # Uncomment the next two lines to enable the admin:
@@ -22,4 +24,6 @@ urlpatterns = [
 
     url(r'^$', App.views.index, name='index'),
     url(r'^home$', App.views.index, name='home'),
+    url(r'^EncryptPage$', App.views.EncryptPage, name='EncryptPage'),
+    url(r'^DecryptPage$', App.views.DecryptPage, name='DecryptPage')
 ]
